42279e25d4d46e7327b9907781ca4302ce07c5a3
Dominic Hui <dominichui112@gmail.com>
2019-07-24 15:59:36 +0100

    created tests for user creation and post entry - pass

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+from .models import Post
+
+class BlogTests(TestCase):
+
+	@classmethod
+	def setUpTestData(cls):
+	# create a user	
+		testuser1 = User.objects.create_user(
+			username = 'testuser1',
+			password = 'thisisatest1'
+			)
+		testuser1.save()
+
+	# create blog post 
+		test_post = Post.objects.create(
+			author = testuser1,
+			title = 'Blog title',
+			body = 'some text...',
+			)
+		test_post.save()
+
+
+	def test_blog_content(self):
+		post = Post.objects.get(id=1)
+		expected_author=f'{post.author}'
+		expected_title = f'{post.title}'
+		expected_body = f'{post.body}'
+		self.assertEqual(expected_author, 'testuser1')
+		self.assertEqual(expected_title, 'Blog title')
+		self.assertEqual(expected_body, 'some text...')
+
